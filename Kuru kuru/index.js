12a97ec7ae20f3d3ae0e3aceff29ba23982b75bc
GeneralByte171 <56396084+GeneralByte@users.noreply.github.com>
2024-06-17 19:43:44 -0600

    First Commit

---
+++ Kuru kuru/index.js
@@ -0,0 +1,19 @@
+var id = null;
+function Move() {
+  var elem = document.getElementById("Move");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 470) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    } if (pos = 470) {
+        pos--;
+        elem.style.left = pos - 'px';
+    }
+  }
+}
